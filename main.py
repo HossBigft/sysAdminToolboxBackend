3818f2a775c093e19fb3e4a4bdb3a0cd36c13ff3
Maxim <m.glushchenko@office.hoster.kz>
2024-10-07 17:15:06 +0500

    Add test crude code for a recrod resolution

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pFindPleskHostByDomain import resolve_a_record, RecordNotFoundError
 
 app = FastAPI()
 
@@ -6,3 +7,12 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/resolve-a-record/{domain}")
+async def get_a_record(domain: str):
+    try:
+        a_records = resolve_a_record(domain)
+        return {"domain": domain, "A_records": a_records}
+    except RecordNotFoundError as e:
+        raise HTTPException(status_code=404, detail=str(e))
