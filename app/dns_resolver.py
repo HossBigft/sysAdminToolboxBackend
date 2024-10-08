21f50f1070e8afa8a5cc00c220c78b6a81cf545f
Maxim <m.glushchenko@office.hoster.kz>
2024-10-08 14:51:17 +0500

    Move files to app dir

--- app/dns_resolver.py
+++ app/dns_resolver.py
@@ -1,18 +1,20 @@
 from dns import resolver, reversename
 from typing import List
 
+
 class RecordNotFoundError(Exception):
     def __init__(self, message):
         super().__init__(message)
 
-def resolve_a_record(domain:str) -> List[str]:
+
+def resolve_a_record(domain: str) -> List[str]:
     try:
         return [ipval.to_text() for ipval in resolver.resolve(domain, "A")]
     except (resolver.NoAnswer, resolver.NXDOMAIN, resolver.NoNameservers) as exc:
         raise RecordNotFoundError(f"A record not found for {domain}") from exc
 
 
-def resolve_ptr_record(ip:str) -> str:
+def resolve_ptr_record(ip: str) -> str:
     try:
         addr_record = reversename.from_address(ip)
         return str(resolver.resolve(addr_record, "PTR")[0])
@@ -25,5 +27,5 @@ def resolve_mx_record(domain) -> List[str]:
         return "".join(
             [ipval.to_text() for ipval in resolver.resolve(domain, "MX")]
         ).split(" ")[1]
-    except (resolver.NoAnswer, resolver.NXDOMAIN) as exc:
+    except (resolver.NoAnswer, resolver.NXDOMAIN, resolver.NoNameservers) as exc:
         raise RecordNotFoundError(f"MX record not found for {domain}") from exc
