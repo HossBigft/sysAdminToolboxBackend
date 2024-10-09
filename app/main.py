8144eda1a522408c24a8a297810efc9125d44c52
Maksim Gluwenko <maksim_gluwenko@epam.com>
2024-10-09 22:59:50 +0500

    Add IP address validation

--- app/main.py
+++ app/main.py
@@ -2,9 +2,10 @@ from fastapi import FastAPI, HTTPException, Path
 from app.dns_resolver import resolve_record, RecordNotFoundError
 import uvicorn
 from typing import Annotated
+from pydantic.networks import IPvAnyAddress
 
 DOMAIN_REGEX_PATTERN = (
-    "^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$"
+    r"^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$"
 )
 
 app = FastAPI()
@@ -16,7 +17,9 @@ async def root():
 
 
 @app.get("/resolve-a/{domain}")
-async def get_a_record(domain: Annotated[str, Path(max_length=63, regex=DOMAIN_REGEX_PATTERN)]):
+async def get_a_record(
+    domain: Annotated[str, Path(max_length=63, regex=DOMAIN_REGEX_PATTERN)],
+):
     try:
         a_records = resolve_record(domain, "A")
         return {"domain": domain, "value": a_records}
@@ -25,9 +28,10 @@ async def get_a_record(domain: Annotated[str, Path(max_length=63, regex=DOMAIN_R
 
 
 @app.get("/resolve-ptr/{ip}")
-async def get_ptr_record(ip: str):
+async def get_ptr_record(
+    ip: IPvAnyAddress,
+):
     try:
-        print(f"IP {ip}")
         ptr_records = resolve_record(str(ip), "PTR")
         return {"ip": ip, "value": ptr_records}
     except RecordNotFoundError as e:
@@ -35,7 +39,9 @@ async def get_ptr_record(ip: str):
 
 
 @app.get("/resolve-mx/{domain}")
-async def get_mx_record(domain: Annotated[str, Path(max_length=63, regex=DOMAIN_REGEX_PATTERN)]):
+async def get_mx_record(
+    domain: Annotated[str, Path(max_length=63, regex=DOMAIN_REGEX_PATTERN)],
+):
     try:
         mx_records = resolve_record(domain, "MX")
         return {"domain": domain, "value": mx_records}
