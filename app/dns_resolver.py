37a8d3fb17933ea15471c52d87f77d5bb5deaba2
Maxim <m.glushchenko@office.hoster.kz>
2024-10-10 18:21:18 +0500

    dns_resolver.py: specify dns servers

--- app/dns_resolver.py
+++ app/dns_resolver.py
@@ -6,17 +6,29 @@ class RecordNotFoundError(Exception):
         super().__init__(message)
 
 
-def resolve_record(record: str, type: str):
+def resolve_record(record: str, type: str, dns_list="hoster"):
+    custom_resolver = resolver.Resolver()
+    match dns_list:
+        case "hoster":
+            custom_resolver.nameservers = [
+example.com
+example.com
+example.com
+            ]
+        case "free":
+            custom_resolver.nameservers = ["IP_PLACEHOLDER", "IP_PLACEHOLDER"]
     try:
         match type:
             case "A":
-                return [ipval.to_text() for ipval in resolver.resolve(record, type)]
+                return [
+                    ipval.to_text() for ipval in custom_resolver.resolve(record, type)
+                ]
             case "PTR":
                 addr_record = reversename.from_address(record)
-                return str(resolver.resolve(addr_record, type)[0])
+                return str(custom_resolver.resolve(addr_record, type)[0])
             case "MX":
                 return "".join(
-                    [ipval.to_text() for ipval in resolver.resolve(record, "MX")]
+                    [ipval.to_text() for ipval in custom_resolver.resolve(record, "MX")]
                 ).split(" ")[1]
             case _:
                 raise rdatatype.UnknownRdatatype
