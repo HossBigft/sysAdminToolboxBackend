4b5551a3b9a9e4af9e495ce0c87c0613b065804b
Maksim Gluwenko <maksim_gluwenko@epam.com>
2024-10-15 08:03:29 +0500

    Add test for ssh hello

--- app/test_main.py
+++ app/test_main.py
@@ -5,18 +5,34 @@ example.com
 MALFORMED_DOMAIN = "googlecom."
 
 
-client=TestClient(app)
+client = TestClient(app)
+
 
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
-    
+
+
 def test_a_record_resolution_with_correct_domain_name():
     response = client.get(f"/resolve-a/{DOMAIN_WITH_EXISTING_A_RECORD}")
     assert response.status_code == 200
-    assert response.json() == {"domain": DOMAIN_WITH_EXISTING_A_RECORD, "value": ["IP_PLACEHOLDER"]}
-    
+    assert response.json() == {
+        "domain": DOMAIN_WITH_EXISTING_A_RECORD,
+        "value": ["IP_PLACEHOLDER"],
+    }
+
+
 def test_a_record_resolution_with_malformed_domain_name():
     response = client.get(f"/resolve-a/{MALFORMED_DOMAIN}")
     assert response.status_code == 422
+
+
+def test_ssh_connection():
+    correct_response_from_ssh = {
+        "host": "vgruzo",
+        "stdout": "Hello",
+    }
+    response = client.get("/plesk/greet")
+    assert response.status_code == 200
+    assert correct_response_from_ssh in response.json() 
