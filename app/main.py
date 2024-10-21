d1905245a6fb9869ac7e9d08a183f1a01c94e001
Maksim Gluwenko <maksim_gluwenko@epam.com>
2024-10-21 20:26:02 +0500

    Add get_current_user dependency

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from .ssh_zone_master import getDomainZoneMasterAsync
 from .ssh_plesk_subscription_info_retriever import query_domain_info
 from .plesk_queries import send_hello
 from fastapi.security import OAuth2PasswordBearer
-
+from pydantic import BaseModel
 
 DOMAIN_REGEX_PATTERN = (
     r"^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$"
@@ -23,6 +23,14 @@ async def validate_domain_name(
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
 
 app = FastAPI()
 
@@ -93,6 +101,22 @@ async def find_plesk_subscription_by_domain(
 @app.get("/items/")
 async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
     return {"token": token}
+
+
+def fake_decode_token(token):
+    return User(
+        username=token + "fakedecoded", email="john@example.com", full_name="John Doe"
+    )
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    user = fake_decode_token(token)
+    return user
+
+
+@app.get("/users/me")
+async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]):
+    return current_user
 
 
 if __name__ == "__main__":
