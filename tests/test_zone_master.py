bcfc435e05325a0b1f84aefd997641d1f71f95de
Maksim Gluwenko <maksim_gluwenko@epam.com>
2024-10-25 17:55:36 +0500

    Add unit tests for zone master command builder

--- tests/test_zone_master.py
+++ tests/test_zone_master.py
@@ -1,4 +1,7 @@
-from app.ssh_zone_master import is_valid_domain
+from app.ssh_zone_master import (
+    is_valid_domain,
+    build_zone_master_command,
+)
 from .test_data.hosts import HostList
 import pytest
 
@@ -25,3 +28,33 @@ invalid_domains = [
 @pytest.mark.parametrize("domain", invalid_domains)
 def test_invalid_domain(domain):
     assert not is_valid_domain(domain)
+
+
+def test_basic_domain(domain=HostList.CORRECT_EXISTING_DOMAIN):
+    expected = (
+        r"cat /var/opt/isc/scls/isc-bind/zones/_default.nzf | "
+        f"grep {domain} | "
+        r"grep -Po '((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\\b){{4}}' | "
+        "head -n1"
+    )
+    assert build_zone_master_command(domain) == expected
+
+
+def test_special_characters(domain=HostList.MALFORMED_DOMAIN):
+    expected = (
+        r"cat /var/opt/isc/scls/isc-bind/zones/_default.nzf | "
+        f"grep {domain} | "
+        r"grep -Po '((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\\b){{4}}' | "
+        "head -n1"
+    )
+    assert build_zone_master_command(domain) == expected
+
+
+def test_lowercase_conversion(domain=HostList.CORRECT_EXISTING_DOMAIN.upper()):
+    expected = (
+        r"cat /var/opt/isc/scls/isc-bind/zones/_default.nzf | "
+        f"grep {domain.lower()} | "
+        r"grep -Po '((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\\b){{4}}' | "
+        "head -n1"
+    )
+    assert build_zone_master_command(domain) == expected
