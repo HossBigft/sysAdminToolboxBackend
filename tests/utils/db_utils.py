552bbadbcc6813ee40b74c5bb87fba5ea04fb476
Maksim Gluwenko <maksim_gluwenko@epam.com>
2024-10-31 22:40:12 +0500

    Rewrite test db utils to generate maridbcontainer and run cli sql query on it

--- tests/utils/db_utils.py
+++ tests/utils/db_utils.py
@@ -1,81 +1,92 @@
-from sqlmodel import Field, Session, SQLModel, create_engine, text
+from sqlmodel import Field, Session, SQLModel, create_engine
 from testcontainers.mysql import MySqlContainer
-
 
 TEST_DB_CMD = "mariadb -B --disable-column-names -p'test' -D'test' -e"
 
 
 class Clients(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
-    pname: str = Field(..., max_length=255)  # Use max_length for VARCHAR
-    login: str = Field(..., max_length=255)  # Use max_length for VARCHAR
+    pname: str = Field(..., max_length=255)
+    login: str = Field(..., max_length=255)
 
 
-# Define the Domain model
 class Domains(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
-    name: str = Field(..., max_length=255)  # Specify length
+    name: str = Field(..., max_length=255)
     webspace_id: int
     cl_id: int = Field(default=None, foreign_key="clients.id")
 
 
-# Create the tables in the database
-def __create_db_and_tables(engine):
-    SQLModel.metadata.create_all(engine)
+class TestMariadb:
+    __test__ = False
 
+    def __init__(self):
+        self.container = MySqlContainer("mariadb:latest")
+        self.container.start()
+        self.engine = create_engine(self.container.get_connection_url())
 
-def __insert_sample_data(engine):
-    with Session(engine) as session:
-        # Create sample clients
-        client_b = Clients(pname="FIO", login="p-2342343")
-        session.add(client_b)
-        session.commit()
-        # Create the main domain with a specific ID
-        main_domain = Domains(
-google.com
-        )
-        session.add(main_domain)
-        # Create subdomains with specific IDs, all linked to the main domain ID
-        subdomains = [
-            Domains(
-google.com
-            ),
-            Domains(
-google.com
-            ),
-            Domains(
-google.com
-            ),
-            Domains(
-                id=1188,
-google.com
-                webspace_id=1184,
-                cl_id=client_b.id,
-            ),
-google.com
-            Domains(
-google.com
-            ),
-            Domains(
-google.com
-            ),
-google.com
-            Domains(
-google.com
-            ),
-        ]
-        session.add_all(subdomains)
-        session.commit()
+    def __create_db_and_tables(self):
+        SQLModel.metadata.create_all(self.engine)
 
+    def __insert_sample_data(self):
+        with Session(self.engine) as session:
+            client_b = Clients(pname="FIO", login="p-2342343")
+            session.add(client_b)
+            session.commit()
 
-def populate_test_db():
-    with MySqlContainer("mariadb:latest") as mysql:
-        mariadb_url = mysql.get_connection_url()
-        engine = create_engine(mariadb_url)
-        __create_db_and_tables(engine)
-        __insert_sample_data(engine)
-        return engine
+            main_domain = Domains(
+google.com
+            )
+            session.add(main_domain)
 
+            subdomains = [
+                Domains(
+google.com
+                ),
+                Domains(
+                    id=1186,
+google.com
+                    webspace_id=1184,
+                    cl_id=client_b.id,
+                ),
+                Domains(
+google.com
+                ),
+                Domains(
+                    id=1188,
+google.com
+                    webspace_id=1184,
+                    cl_id=client_b.id,
+                ),
+                Domains(
+google.com
+                ),
+                Domains(
+                    id=1190,
+google.com
+                    webspace_id=1184,
+                    cl_id=client_b.id,
+                ),
+                Domains(
+google.com
+                ),
+                Domains(
+google.com
+                ),
+                Domains(
+google.com
+                ),
+            ]
+            session.add_all(subdomains)
+            session.commit()
 
-if __name__ == "__main__":
-    engine = populate_test_db()
+    def populate_db(self):
+        self.__create_db_and_tables()
+        self.__insert_sample_data()
+        return self
+
+    def run_query(self, query: str) -> str:
+        return self.container.exec(f'{TEST_DB_CMD}"{query}"').output.decode("utf-8")
+
+    def cleanup(self):
+        self.container.stop()
