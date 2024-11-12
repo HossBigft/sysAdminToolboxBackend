a833ae8c561c73bce295efe4e7118b4d6c73d108
Maksim Gluwenko <maksim_gluwenko@epam.com>
2024-11-12 21:58:16 +0500

    Add admin field to user table

--- app/models.py
+++ app/models.py
@@ -31,6 +31,7 @@ class UserBase(SQLModel):
     is_active: bool = True
     is_superuser: bool = False
     full_name: str | None = Field(default=None, max_length=255)
+    admin: bool = False
 
 
 # Properties to receive via API on creation
