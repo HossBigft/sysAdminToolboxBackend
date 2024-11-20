1e60a37c78e5166c77768c68069a9810796980e0
Maxim <m.glushchenko@office.hoster.kz>
2024-11-20 16:28:29 +0500

    Suppress ssh host added message

--- app/ssh_async_executor.py
+++ app/ssh_async_executor.py
@@ -1,27 +1,41 @@
 import asyncio
 from typing import List, Dict
+import time
+
 
 async def _run_command_over_ssh(host, command, verbose: bool):
-    ssh_command = f'ssh  -o PasswordAuthentication=no  {host} "{command}"'
+    start_time = time.time()
+
+    ssh_command = f'ssh -q  {host} "{command}"'
     process = await asyncio.create_subprocess_shell(
         ssh_command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
     )
 
     if verbose:
         print(f"{host} {ssh_command}| Awaiting result...")
+
     stdout, stderr = await process.communicate()
+
+    # Record the end time after the command finishes
+    end_time = time.time()
+
+    # Calculate the elapsed time
+    execution_time = end_time - start_time
+
     if verbose:
         succesfulAnswer = stdout.decode().strip().rstrip()
         failAnswer = stderr.decode().strip().rstrip()
         if failAnswer:
             print(f"{host} failed: {failAnswer}")
         else:
-            print(f"{host} answered: {succesfulAnswer}")
+            print(f"{host} answered in {execution_time:.2f}s : {succesfulAnswer}")
 
     return (host, stdout.decode().strip(), stderr.decode().strip(), process.returncode)
 
 
-async def batch_ssh_command_prepare(server_list, command, verbose: bool)-> List[Dict[str, str]]:
+async def batch_ssh_command_prepare(
+    server_list, command, verbose: bool
+) -> List[Dict[str, str]]:
     tasks = [_run_command_over_ssh(host, command, verbose) for host in server_list]
     results = await asyncio.gather(*tasks)
     return [
