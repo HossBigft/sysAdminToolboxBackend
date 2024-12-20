8846b54ae621bc342755b0343a198749efcd95d6
Maxim <m.glushchenko@office.hoster.kz>
2024-12-20 09:49:54 +0500

    Reduce ssh connection lifetime and warup period to 5 min

--- app/ssh_warmup.py
+++ app/ssh_warmup.py
@@ -5,7 +5,7 @@ from app.ssh_async_executor import batch_ssh_command_prepare
 from app.host_lists import PLESK_SERVER_LIST
 
 
-@repeat_every(seconds=60 * 30)
+@repeat_every(seconds=60 * 5)
 async def ssh_warmup() -> None:
     await batch_ssh_command_prepare(
         server_list=PLESK_SERVER_LIST + DNS_SERVER_LIST,
