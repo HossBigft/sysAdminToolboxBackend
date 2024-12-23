4bfa3b826be1e6c2fd0e301af440c2172fecbc0d
Maxim <m.glushchenko@office.hoster.kz>
2024-12-23 14:21:58 +0500

    Make all dns ssh function async

--- app/ssh_zone_master.py
+++ app/ssh_zone_master.py
@@ -9,13 +9,13 @@ DOMAIN_REGEX_PATTERN = (
 )
 
 
-def is_valid_domain(domain_name: str) -> bool:
+async def is_valid_domain(domain_name: str) -> bool:
     return 3 <= len(domain_name) <= 63 and bool(
         re.match(DOMAIN_REGEX_PATTERN, domain_name)
     )
 
 
-def build_zone_master_command(domain_name: str) -> str:
+async def build_zone_master_command(domain_name: str) -> str:
     escaped_domain = shlex.quote(f'\\"{domain_name.lower()}\\"')
     return (
         f"cat {ZONEFILE_PATH} | "
@@ -34,9 +34,9 @@ async def batch_ssh_execute(cmd: str):
 
 
 async def getDomainZoneMaster(domain_name: str):
-    if not is_valid_domain(domain_name):
+    if not await is_valid_domain(domain_name):
         raise ValueError("Input string should be a valid domain name.")
-    getZoneMasterCmd = build_zone_master_command(domain_name)
+    getZoneMasterCmd = await build_zone_master_command(domain_name)
     dnsAnswers = await batch_ssh_execute(getZoneMasterCmd)
     dnsAnswers = [
         {"ns": answer["host"], "zone_master": answer["stdout"]}
@@ -54,7 +54,7 @@ async def build_remove_zone_master_command(domain_name: str) -> None:
 
 
 async def remove_domain_zone_master(domain_name: str):
-    if not is_valid_domain(domain_name):
+    if not await is_valid_domain(domain_name):
         raise ValueError("Input string should be a valid domain name.")
     rm_zone_master_md = await build_remove_zone_master_command(domain_name)
     dnsAnswers = await batch_ssh_execute(rm_zone_master_md)
