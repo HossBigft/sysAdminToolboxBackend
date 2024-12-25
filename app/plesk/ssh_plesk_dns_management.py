634e82e6c07fc236860acfa64ec8a41321d2eb8a
Maxim <m.glushchenko@office.hoster.kz>
2024-12-25 16:35:44 +0500

    Add domain existance check function and fix model related code

--- app/plesk/ssh_plesk_dns_management.py
+++ app/plesk/ssh_plesk_dns_management.py
@@ -3,7 +3,7 @@ import shlex
 
 from app.host_lists import PLESK_SERVER_LIST
 from app.AsyncSSHandler import execute_ssh_command
-from app.models import DomainName, SubscriptionName
+from app.plesk.models import DomainName, SubscriptionName
 
 
 class PleskServiceError(Exception):
@@ -27,24 +27,30 @@ class CommandExecutionError(PleskServiceError):
         super().__init__(f"Command failed with return code {return_code}: {stderr}")
 
 
-async def build_restart_dns_service_command(domain: str) -> str:
-    escaped_domain = shlex.quote(f'\\"{domain.lower()}\\"')
+async def build_restart_dns_service_command(domain: DomainName) -> str:
+    escaped_domain = shlex.quote(f'\\"{domain.domain.lower()}\\"')
     return (
         f"plesk bin dns --off {escaped_domain} && plesk bin dns --on {escaped_domain}"
     )
 
 
+async def is_domain_exist_on_server(host: DomainName, domain: SubscriptionName) -> bool:
+    get_subscription_name_cmd = f'plesk db -Ne "SELECT name FROM domains WHERE webspace_id=0 AND id={domain.domain}"'
+    result = await execute_ssh_command(host.domain, get_subscription_name_cmd)
+    subscription_name = result["stdout"]
+    return not subscription_name == ""
+
 async def restart_dns_service_for_domain(
     host: DomainName, domain: SubscriptionName
 ) -> None:
     if host in PLESK_SERVER_LIST:
-        restart_dns_cmd = await build_restart_dns_service_command(domain=domain)
+        restart_dns_cmd = await build_restart_dns_service_command(domain=domain.domain)
         result = await execute_ssh_command(
-            host=host, command=restart_dns_cmd, verbose=True
+            host=host.domain, command=restart_dns_cmd, verbose=True
         )
         match result["returncode"]:
             case "4":
-                raise DomainNotFoundError(f"Domain {domain} does not exist on server")
+                raise DomainNotFoundError(f"Domain {domain.domain} does not exist on server")
             case "0":
                 pass
             case _:
@@ -53,4 +59,6 @@ async def restart_dns_service_for_domain(
                 )
 
     else:
-        raise ValueError(f"{host} is not valid Plesk server")
+        raise ValueError(f"{host.domain} is not valid Plesk server")
+    
+
