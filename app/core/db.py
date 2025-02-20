011d9b99d0655aa3544ff337d731be04d4376b06
Maxim <m.glushchenko@office.hoster.kz>
2025-02-20 11:16:45 +0500

    Fix initial table setup

--- app/core/db.py
+++ app/core/db.py
@@ -4,24 +4,13 @@ from sqlalchemy.orm import Session
 from app import crud
 from app.core.config import settings
 from app.schemas import UserCreate, UserRoles
-from app.db.models import User
+from app.db.models import User, Base
 
 engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI), echo=False)
 
 
-# make sure all SQLModel models are imported (app.models) before initializing DB
-# otherwise, SQLModel might fail to initialize relationships properly
-# for more details: https://github.com/fastapi/full-stack-fastapi-template/issues/28
-
-
 def init_db(session: Session) -> None:
-    # Tables should be created with Alembic migrations
-    # But if you don't want to use migrations, create
-    # the tables un-commenting the next lines
-    from sqlmodel import SQLModel
-
-    # This works because the models are already imported and registered from app.models
-    SQLModel.metadata.create_all(engine)
+    Base.metadata.create_all(engine)
 
     user = session.exec(
         select(User).where(User.email == settings.FIRST_SUPERUSER)
@@ -30,6 +19,6 @@ def init_db(session: Session) -> None:
         user_in = UserCreate(
             email=settings.FIRST_SUPERUSER,
             password=settings.FIRST_SUPERUSER_PASSWORD,
-            role=UserRoles.SUPERUSER
+            role=UserRoles.SUPERUSER,
         )
         user = crud.create_user(session=session, user_create=user_in)
