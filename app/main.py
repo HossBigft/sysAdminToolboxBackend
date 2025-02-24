feb577e3aaeaf48470feeb93b9bc676c8d77d805
Maksim Gluwenko <maksim_gluwenko@epam.com>
2025-02-24 21:21:21 +0500

    Merge api main into app main

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
 import sentry_sdk
-from fastapi import FastAPI
+import logging
+
+from fastapi import FastAPI, APIRouter
 from fastapi.routing import APIRoute
 from starlette.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 
-from app.api.main import api_router
 from app.core.config import settings
 from app.ssh_warmup import ssh_warmup
+from app.api.users import users_router as users
+from app.api.auth import password_reset, auth_router as login
+from app.api.dns import dns_router as dns
+from app.api.plesk import plesk_router as plesk
+from app.api import utils_router as utils
 
 
 def custom_generate_unique_id(route: APIRoute) -> str:
@@ -40,4 +46,25 @@ if settings.all_cors_origins:
         allow_headers=["*"],
     )
 
-app.include_router(api_router, prefix=settings.API_V1_STR)
+
+router = APIRouter(prefix=settings.API_V1_STR)
+
+router.include_router(dns.router)
+router.include_router(users.router)
+router.include_router(plesk.router)
+router.include_router(utils.router)
+router.include_router(password_reset.router)
+router.include_router(login.router)
+
+app.include_router(router)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler("actions.log"),  # Save logs to app.log
+        logging.StreamHandler(),  # Also log to console
+    ],
+)
+
+logger = logging.getLogger(__name__)
