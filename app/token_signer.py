e449d6efd3170077b4efa5f966e0d4c95be1b350
Maksim Gluwenko <maksim_gluwenko@epam.com>
2025-05-09 21:29:48 +0500

    Make signed token executor save test key to the file

--- app/token_signer.py
+++ app/token_signer.py
@@ -2,9 +2,12 @@ import base64
 import time
 import secrets
 import json
+import os
 
 from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
 from cryptography.hazmat.primitives import serialization
+
+from app.core.config import settings
 
 EXPIRATION_PERIOD_SECONDS = 900
 
@@ -13,7 +16,19 @@ class ToKenSigner:
     def __init__(self):
         self._private_key = Ed25519PrivateKey.generate()
 
-    def _sign_message(self, data, ):
+        if settings.ENVIRONMENT == "local":
+            self._write_raw_public_key_to_file()
+
+    def _write_raw_public_key_to_file(self):
+        os.makedirs("/app/test_token_key", exist_ok=True)
+        raw_key_bytes = self.get_raw_public_key_bytes()
+        
+        raw_key_base64 = base64.b64encode(raw_key_bytes).decode('utf-8')
+        
+        with open("/app/test_token_key/pub.key", "w") as f:
+            f.write(raw_key_base64)
+
+    def _sign_message(self, data):
         signature = self._private_key.sign(data.encode())
         return base64.b64encode(signature).decode()
 
@@ -35,12 +50,11 @@ class ToKenSigner:
         token_data["signature"] = signature
 
         signed_token_json = json.dumps(token_data)
-
         encoded_json = base64.b64encode(signed_token_json.encode('utf-8')).decode('utf-8')
         return encoded_json
 
-    def get_public_key_pem(self):
+    def get_raw_public_key_bytes(self):
         return self._private_key.public_key().public_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PublicFormat.SubjectPublicKeyInfo,
+            encoding=serialization.Encoding.Raw,
+            format=serialization.PublicFormat.Raw,
         )
