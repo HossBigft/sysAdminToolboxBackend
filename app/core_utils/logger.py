634739654f871e40173cea40b934ce57c30d9f1b
Maksim Gluwenko <maksim_gluwenko@epam.com>
2025-05-16 13:48:38 +0500

    Remove comments

--- app/core_utils/logger.py
+++ app/core_utils/logger.py
@@ -2,8 +2,8 @@ import logging
 from datetime import datetime, timedelta
 from app.schemas import UserActionType
 
+
 def round_up_seconds(dt):
-    """Round up to the next second if microseconds exist, otherwise keep the same second."""
     if dt.microsecond > 0:
         return (dt + timedelta(seconds=1)).replace(microsecond=0)
     return dt.replace(microsecond=0)
@@ -17,21 +17,18 @@ class CompactDockerFormatter(logging.Formatter):
         level = record.levelname
 
         try:
-            # Extract useful information
+
             client = record.args.get("client", "-")
             status = record.args.get("status_code", "-")
             request_line = record.args.get("request_line", "-")
             response_time = record.args.get("response_time", "-")
 
-            # Format in a grep-friendly way with field identifiers
             return f'{timestamp} level={level} client={client} status={status} req="{request_line}" time={response_time}ms'
         except:
-            # Fallback format
             return f'{timestamp} level={level} msg="{record.getMessage()}"'
 
 
 def setup_uvicorn_logger():
-    """Configure the uvicorn.access logger with the compact formatter."""
     logger = logging.getLogger("uvicorn.access")
     logger.handlers.clear()
     handler = logging.StreamHandler()
@@ -41,17 +38,15 @@ def setup_uvicorn_logger():
 
 
 def setup_actios_logger():
-    """Configure application-specific loggers"""
-    # Create app logger
     app_logger = logging.getLogger("app.user_actions")
     app_logger.setLevel(logging.INFO)
 
 
 def log_plesk_login_link_get(
-    user,
-    plesk_server: str,
-    subscription_id: int,
-    ip: str,
+        user,
+        plesk_server: str,
+        subscription_id: int,
+        ip: str,
 ):
     app_logger = logging.getLogger("app.user_actions")
     log_message = (
