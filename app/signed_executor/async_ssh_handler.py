9251c16c7a9b48ba9f82b27bc76f4bd6a0859073
Maksim Gluwenko <maksim_gluwenko@epam.com>
2025-05-20 11:12:07 +0500

    Add handling for case when ssh fails to connect

--- app/signed_executor/async_ssh_handler.py
+++ app/signed_executor/async_ssh_handler.py
@@ -6,11 +6,18 @@ from app.schemas import SshResponse
 from app.core.config import settings
 
 
+class SshAccessDeniedError(Exception):
+    def __init__(self, host: str, message: str | None):
+        super().__init__(f"SSH access denied for {host}: {message}")
+        self.host = host
+        self.message = message
+
+
 async def _execute_ssh_command(host, command) -> SshResponse:
     verbose: bool = settings.ENVIRONMENT == "local"
     start_time = time.time()
 
-    ssh_command = f'ssh -q  {host} "{command}"'
+    ssh_command = f'ssh {host} "{command}"'
     process = await asyncio.create_subprocess_shell(
         ssh_command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
     )
@@ -21,27 +28,47 @@ async def _execute_ssh_command(host, command) -> SshResponse:
     stdout, stderr = await process.communicate()
 
     end_time = time.time()
+
     execution_time = end_time - start_time
-
-    if verbose:
-        succesfulAnswer = stdout.decode().strip().rstrip()
-        failAnswer = stderr.decode().strip().rstrip()
-        if failAnswer:
-            print(f"{host} failed in {execution_time:.2f}s : {failAnswer}")
-        else:
-            print(f"{host} answered in {execution_time:.2f}s : {succesfulAnswer}")
-
     stdout_output: str | None = (
         stdout.decode().strip() if stdout.decode().strip() != "" else None
     )
     stderr_output: str | None = (
         stderr.decode().strip() if stderr.decode().strip() != "" else None
     )
+    filtered_stderr_output = None
+    if stderr_output:
+        stderr_lines = stderr_output.splitlines()
+        filtered_stderr_output = "\n".join(
+            line
+            for line in stderr_lines
+            if not line.lower().startswith("Warning: Permanently added".lower())
+        )
     returncode_output: int | None = process.returncode
+
+    if returncode_output != 0 or (
+        filtered_stderr_output and "permission denied" in filtered_stderr_output.lower()
+    ):
+        if verbose:
+            print(
+                f"Failed to connect {host} over SSH ({execution_time:.2f}s): {filtered_stderr_output}"
+            )
+        raise SshAccessDeniedError(host, filtered_stderr_output)
+
+    if verbose:
+        if returncode_output != 0:
+            print(
+                f"{host} COMMAND FAILED ({execution_time:.2f}s): {filtered_stderr_output or '(no error output)'}"
+            )
+        else:
+            print(
+                f"{host} OK ({execution_time:.2f}s): {stdout_output or '(no output)'}"
+            )
+
     return {
         "host": host,
         "stdout": stdout_output,
-        "stderr": stderr_output,
+        "stderr": filtered_stderr_output,
         "returncode": returncode_output,
     }
 
