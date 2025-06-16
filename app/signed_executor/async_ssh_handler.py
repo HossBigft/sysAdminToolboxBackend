6e8a176201a1cf68847342fd910ca940a0a9ec0e
Maxim <m.glushchenko@office.hoster.kz>
2025-06-16 09:34:53 +0500

    Formatting

--- app/signed_executor/async_ssh_handler.py
+++ app/signed_executor/async_ssh_handler.py
@@ -11,15 +11,21 @@ from app.core.config import settings
 
 _connection_pool = {}
 
-SSH_LOGIN_TIMEOUT=3
-SSH_EXECUTION_TIMEOUT=1
+SSH_LOGIN_TIMEOUT = 3
+SSH_EXECUTION_TIMEOUT = 1
+
 
 async def _get_connection(host: str):
     if host in _connection_pool.keys():
         return _connection_pool[host]
     else:
         host_ip = str(HOSTS.resolve_domain(host).ips[0])
-        _connection_pool[host] = await asyncssh.connect(host_ip, username=settings.SSH_USER, known_hosts=None, login_timeout=SSH_LOGIN_TIMEOUT)
+        _connection_pool[host] = await asyncssh.connect(
+            host_ip,
+            username=settings.SSH_USER,
+            known_hosts=None,
+            login_timeout=SSH_LOGIN_TIMEOUT,
+        )
         return _connection_pool[host]
 
 
@@ -34,15 +40,21 @@ async def _execute_ssh_command(host: str, command: str) -> SshResponse:
     try:
         conn = await _get_connection(host)
         start_time = time.time()
-        result = await asyncio.wait_for(conn.run(command), timeout= SSH_EXECUTION_TIMEOUT)
+        result = await asyncio.wait_for(
+            conn.run(command), timeout=SSH_EXECUTION_TIMEOUT
+        )
         end_time = time.time()
         execution_time = end_time - start_time
 
         stdout_output: str | None = (
-            result.stdout.strip() if result.stdout and result.stdout.strip() != "" else None
+            result.stdout.strip()
+            if result.stdout and result.stdout.strip() != ""
+            else None
         )
         stderr_output: str | None = (
-            result.stderr.strip() if result.stderr and result.stderr.strip() != "" else None
+            result.stderr.strip()
+            if result.stderr and result.stderr.strip() != ""
+            else None
         )
 
         filtered_stderr_output = None
@@ -53,7 +65,9 @@ async def _execute_ssh_command(host: str, command: str) -> SshResponse:
                 for line in stderr_lines
                 if not line.lower().startswith("Warning: Permanently added".lower())
             )
-            filtered_stderr_output = filtered_stderr_output if filtered_stderr_output.strip() else None
+            filtered_stderr_output = (
+                filtered_stderr_output if filtered_stderr_output.strip() else None
+            )
 
         returncode_output: int | None = result.exit_status
 
@@ -85,7 +99,10 @@ async def _execute_ssh_command(host: str, command: str) -> SshResponse:
         execution_time = end_time - start_time
 
         error_message = str(e).lower()
-        if "permission denied" in error_message or "authentication failed" in error_message:
+        if (
+            "permission denied" in error_message
+            or "authentication failed" in error_message
+        ):
             raise SshAccessDeniedError(host, str(e))
 
         return {
@@ -97,7 +114,9 @@ async def _execute_ssh_command(host: str, command: str) -> SshResponse:
         }
 
 
-async def execute_ssh_commands_in_batch(server_list: List[str], command: str) -> List[SshResponse]:
+async def execute_ssh_commands_in_batch(
+    server_list: List[str], command: str
+) -> List[SshResponse]:
     tasks = [_execute_ssh_command(host, command) for host in server_list]
 
     results = await asyncio.gather(*tasks, return_exceptions=True)
@@ -106,7 +125,7 @@ async def execute_ssh_commands_in_batch(server_list: List[str], command: str) ->
         if isinstance(result, Exception):
             raise result
         processed_results.append(result)
-    
+
     return processed_results
 
 
