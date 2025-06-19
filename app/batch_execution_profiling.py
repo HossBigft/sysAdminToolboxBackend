e6a016b621af12a2f01a5322ccd60f8b84199e5b
Maxim <m.glushchenko@office.hoster.kz>
2025-06-19 10:24:29 +0500

    Revert "Add debug for run in executor caller"
    This reverts commit 59e47043435bf17b72eabcd3b185a7e3df35fe54.

--- app/batch_execution_profiling.py
+++ app/batch_execution_profiling.py
@@ -1,6 +1,5 @@
 import asyncio
 import time
-import traceback
 from scalene import scalene_profiler
 
 from app.signed_executor.async_ssh_handler import (
@@ -13,17 +12,6 @@ SLEEP_TIME = 5
 
 
 async def main():
-    loop = asyncio.get_running_loop()
-
-    original_run_in_executor = loop.run_in_executor
-
-    def debug_run_in_executor(executor, func, *args):
-        print("[run_in_executor called]")
-        traceback.print_stack()
-        return original_run_in_executor(executor, func, *args)
-
-    loop.run_in_executor = debug_run_in_executor
-
     print("Initialising connection pool")
     await initialize_connection_pool(PLESK_SERVER_LIST + DNS_SERVER_LIST)
 
@@ -34,14 +22,13 @@ async def main():
     await execute_ssh_commands_in_batch(
         PLESK_SERVER_LIST + DNS_SERVER_LIST, command="status"
     )
-
+    
     scalene_profiler.start()
     print("Running requests on warmed connections")
     await execute_ssh_commands_in_batch(
         PLESK_SERVER_LIST + DNS_SERVER_LIST, command="status"
     )
     scalene_profiler.stop()
-
 
 if __name__ == "__main__":
     asyncio.run(main())
