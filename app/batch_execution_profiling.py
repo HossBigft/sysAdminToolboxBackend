fe6f7832550bf1703c53172e0bfe463b3fc47698
Maxim <m.glushchenko@office.hoster.kz>
2025-06-19 11:41:48 +0500

    Formatting

--- app/batch_execution_profiling.py
+++ app/batch_execution_profiling.py
@@ -3,8 +3,6 @@ import time
 from scalene import scalene_profiler
 
 
-
-
 from app.signed_executor.async_ssh_handler import (
     execute_ssh_commands_in_batch,
     initialize_connection_pool,
@@ -18,22 +16,20 @@ async def main():
     print("Initialising connection pool")
     server_list = PLESK_SERVER_LIST + DNS_SERVER_LIST
     server_list = server_list
+
     await initialize_connection_pool(server_list)
 
     print(f"Sleeping {SLEEP_TIME}s. to cool the CPU")
     time.sleep(SLEEP_TIME)
-    
+
     print("Running requests on cold connections")
-    await execute_ssh_commands_in_batch(
-        server_list, command="status"
-    )
-    
+    await execute_ssh_commands_in_batch(server_list, command="status")
+
     scalene_profiler.start()
     print("Running requests on warmed connections")
-    await execute_ssh_commands_in_batch(
-        server_list, command="status"
-    )
+    await execute_ssh_commands_in_batch(server_list, command="status")
     scalene_profiler.stop()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
